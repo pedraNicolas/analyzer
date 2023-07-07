8939eb3e10ea03c461bd1255144ed0af8ca64e56
Pablo Cabrera <pablo.david.cabrera@gmail.com>
2023-07-06 23:28:31 -0300

    DA-16

--- app/src/main/java/com/psijuego/ui/views/report/indicators/IndicatorsFragment.kt
+++ app/src/main/java/com/psijuego/ui/views/report/indicators/IndicatorsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.psijuego.R
 import com.psijuego.databinding.FragmentIndicatorsBinding
 
 class IndicatorsFragment : Fragment() {
@@ -16,6 +18,17 @@ class IndicatorsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentIndicatorsBinding.inflate(inflater, container, false)
+        setUpComponents()
         return binding.root
+    }
+
+    private fun setUpComponents(){
+        binding.btnNext.setOnClickListener{
+            setUpNavigation(it)
+        }
+    }
+    
+    private fun setUpNavigation(view:View){
+        Navigation.findNavController(view).navigate(R.id.action_indicatorsFragment_to_conclusionsFragment)
     }
 }
