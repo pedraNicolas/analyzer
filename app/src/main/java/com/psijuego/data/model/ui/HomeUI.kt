822d1cf3127f2885325812fd792162cc5168d4c5
pedraNicolas <pedranicolas@gmail.com>
2023-07-11 21:08:00 -0300

    DA-28

--- app/src/main/java/com/psijuego/data/model/ui/HomeUI.kt
+++ app/src/main/java/com/psijuego/data/model/ui/HomeUI.kt
@@ -2,7 +2,7 @@ package com.psijuego.data.model.ui
 
 import android.net.Uri
 
-data class WelcomeUI(
+data class HomeUI(
     var namePatient: String = "",
     var nameProfessional: String = "",
     var numberRegistration: String? = null,
--- app/src/main/java/com/psijuego/ui/views/report/indicators/adapter/category/CategoryRvViewHolder.kt
+++ app/src/main/java/com/psijuego/ui/views/report/indicators/adapter/category/CategoryRvViewHolder.kt
@@ -1,31 +1,29 @@
-package com.psijuego.ui.views.report.indicators.adapter.indicator
+package com.psijuego.ui.views.report.indicators.adapter.category
 
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.psijuego.core.Constants
 import com.psijuego.core.utils.UtilText
-import com.psijuego.data.model.ui.IndicatorUI
-import com.psijuego.databinding.IndicatorLayoutBinding
-import com.psijuego.ui.views.report.indicators.IndicatorListener
-import com.psijuego.ui.views.report.indicators.IndicatorsFragment
+import com.psijuego.data.model.ui.CategoryUI
+import com.psijuego.databinding.CategoryLayoutBinding
+import com.psijuego.ui.views.report.indicators.CategoryListener
 import com.psijuego.ui.views.report.indicators.adapter.parameter.ParameterRvAdapter
 
-class IndicatorRvViewHolder(
-    private val binding: IndicatorLayoutBinding
+class CategoryRvViewHolder(
+    private val binding: CategoryLayoutBinding,
+    private val size: Int
 ) : RecyclerView.ViewHolder(binding.root) {
 
     private lateinit var parameterRvAdapter: ParameterRvAdapter
 
     fun render(
-        item: IndicatorUI,
-        indicatorUIPosition: Int,
-        size: Int,
-        listener: IndicatorListener?
+        item: CategoryUI,
+        itemPosition: Int,
+        listener: CategoryListener?
     ) {
         with(binding) {
-            val maxLength = 20
-            headerLayout.setTitle(UtilText.formatTextMaxCharactersPerLine(item.name, maxLength))
+            headerLayout.setTitle(UtilText.formatTextMaxCharactersPerLine(item.name, 20))
             item.parameter.let {
                 for (parameter in it) {
                     if (parameter.name == Constants.DESCRIPTION) {
@@ -40,15 +38,16 @@ class IndicatorRvViewHolder(
                 }
             }
 
-            headerLayout.showBack(position == 0)
-            btnNext.visibility = if (size - 1 == position) View.VISIBLE else View.GONE
+            headerLayout.showBack(itemPosition == 0)
+            btnNext.visibility = if (size - 1 == itemPosition) View.VISIBLE else View.GONE
             btnNext.setOnClickListener {
                 listener?.onNextClicked()
             }
             rvParameters.layoutManager =
                 LinearLayoutManager(rvParameters.context, LinearLayoutManager.VERTICAL, false)
-            val filteredItem = item.parameter.filter { parameterUI ->  parameterUI.name != Constants.DESCRIPTION}
-            parameterRvAdapter = ParameterRvAdapter(filteredItem, indicatorUIPosition)
+            val filteredItem =
+                item.parameter.filter { parameterUI -> parameterUI.name != Constants.DESCRIPTION }
+            parameterRvAdapter = ParameterRvAdapter(filteredItem, itemPosition)
             if (listener != null) {
                 parameterRvAdapter.setListener(listener)
             }
