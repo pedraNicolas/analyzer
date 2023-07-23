4aeca78e37c04418d9e09ddbcdceff4849271c27
pedraNicolas <pedranicolas@gmail.com>
2023-07-23 19:58:59 -0300

    DA-22

--- app/src/main/java/com/psijuego/ui/views/report/MainActivity.kt
+++ app/src/main/java/com/psijuego/ui/views/report/MainActivity.kt
@@ -2,8 +2,10 @@ package com.psijuego.ui.views.report
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.psijuego.R
 import com.psijuego.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,12 +15,39 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-
+    private var count: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+    }
+
+    override fun onBackPressed() {
+        when (navController.currentDestination?.id) {
+            R.id.homeFragment -> {
+                if (count != 1) {
+                    Toast.makeText(
+                        this,
+                        "Pulse \"Atras\" una vez más para salir.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    count++
+                } else {
+                    finish()
+                }
+            }
+
+            R.id.exportReportFragment -> {
+                count = 0
+            }
+
+            else -> {
+                count = 0
+                super.onBackPressed()
+            }
+        }
     }
 }
